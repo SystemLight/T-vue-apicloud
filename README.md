2ebfc5ba1827273f832e02f0e335c00fa43c1efb
SystemLight <1466335092@qq.com>
2020-11-18 16:48:19 +0800

    初始化仓库

---
+++ README.md
@@ -0,0 +1,42 @@
+# T-vue-apicloud
+
+T-vue-apicloud集成vue和vant库允许你在apicloud平台进行更友好的Native开发，该模板项目将提供充分类型提示功能。
+
+## 获取方法
+
+注意本地启动浏览器启动服务只是对页面样式进行浏览，任何apicloud平台的接口都会失效
+
+```
+https://github.com/SystemLight/T-vue-apicloud.git
+无需运行 npm install
+启动服务 npm run start
+```
+
+## 目录结构
+
+|  文件名   | 作用  |
+|  :----  | :----  |
+| types  | vue和vant，apicloud平台的类型声明文件 |
+| views  | 所有页面都放置在这个目录下 |
+| package.json  | 前端工程化依赖管理配置文件 |
+
+## 技术栈
+
+#### 开发依赖
+
+- [x] apicloud
+- [ ] typescript
+- [ ] webpack
+- [ ] eslint
+- [ ] jest
+
+#### 生产依赖
+
+- [x] vue
+    - [ ] vue-router
+    - [ ] vuex
+- [x] vant
+- [ ] less
+- [ ] axios
+- [ ] qs
+- [ ] lodash
---
+++ custom.d.ts
@@ -0,0 +1,28 @@
+/*
+    api.openFrame()
+
+    打开 frame
+    若 frame 已存在，则会把该窗口显示到最前面并显示，如果 url 和之前的 url 有变化，或者 reload 为 true 时，页面会刷新
+    此方法对 frameGroup 里面的 frame 不起作用
+ */
+interface IOpenFrameParams {
+    name: string, // frame名字
+    url: string, // 页面地址，可以为本地文件路径，支持相对路径和绝对路径，以及 widget://、fs://等协议路径，也可以为远程地址。 当data参数不为空时，url将做为baseUrl，data中的html引用的资源文件根路径以该url为基础
+    data?: string, // 页面加载的数据内容，可以为html片段或者整张html文件的数据
+    headers?: {[key: String]: any}, // 请求头
+    avm?: boolean, // 是否使用原生引擎来加载页面，页面必须是使用avm框架语法生成
+    rect?: {x?: number, y?: number, w?: number | string, h?: number | string, marginLeft?: number, marginTop?: number, marginBottom?: number, marginRight?: number} // 设置页面的位置和大小
+}
+
+interface IOpenFrame {
+    (params: IOpenFrameParams): void
+}
+
+// API文档：https://docs.apicloud.com/Client-API/api
+interface IApiCloud {
+    openFrame: IOpenFrame
+}
+
+export interface Window {
+    api: IApiCloud
+}
